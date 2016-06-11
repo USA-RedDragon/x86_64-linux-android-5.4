3b51fc758e808f0393740c019ffc98de721a5cdc
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-06-10 19:06:41 -0500

    GCC 5.4.1 built on 10 June 2016

---
+++ lib/gcc/x86_64-linux-android/5.4.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-upstream/configure --prefix=/root/fugu-gcc5.4 --target=x86_64-linux-android --enable-languages=c,c++ --disable-multilib --with-gnu-as --disable-threads --with-newlib : (reconfigured) ../gcc-upstream/configure --prefix=/root/fugu-gcc5.4 --target=x86_64-linux-android --enable-languages=c,c++ --disable-multilib --with-gnu-as --disable-threads --with-newlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
